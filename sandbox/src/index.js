efcb2cfeca4e2ed305d710374c9f8f757398e4c2
dinazoe <dinazouraus@gmail.com>
2022-09-04 23:20:19 -0400

    adding navbar

--- sandbox/src/index.js
+++ sandbox/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import ServerTable from './serverTable';
+import NavScrollExample from './navBar';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
